10d4c48c32ce58d876cbad8cb212d0d20cfe87ed
Daniel Gonzalez <danielhumgon@gmail.com>
2020-10-07 20:04:21 -0400

    fix(menu): Minor fixes for messages.fullstack.cash

--- src/gatsby-ipfs-web-wallet/components/menu-components.js
+++ src/gatsby-ipfs-web-wallet/components/menu-components.js
@@ -7,28 +7,37 @@
 */
 
 import React from 'react'
+import MenuComponents from 'gatsby-ipfs-web-wallet/src/components/menu-components'
 import { Sidebar } from 'adminlte-2-react'
 import FileUpload from '../../file-upload'
 import Messages from '../../messages'
 import CommunityFeed from '../../community-feed'
 const { Item } = Sidebar
 
-const MenuComponents = [
-  {
-    key: 'File Upload',
-    component: <FileUpload key='File Upload' />,
-    menuItem: <Item icon='fas-file-upload' key='File Upload' text='File Upload' />
-  },
-  {
-    key: 'Messages',
-    component: <Messages key='Messages' />,
-    menuItem: <Item icon='fa-envelope-open' key='Messages' text='Messages' />
-  },
-  {
-    key: 'Community Feed',
-    component: <CommunityFeed key='Community Feed' />,
-    menuItem: <Item icon='fa-users' key='Community Feed' text='Community Feed' />
-  }
-]
+const MenuComponents2 = props => {
+  const DefualtsMenu = MenuComponents(props)
 
-export default MenuComponents
+  const menu = [
+    {
+      active: true,
+      key: 'Community Feed',
+      component: <CommunityFeed key='Community Feed' />,
+      menuItem: <Item icon='fa-users' key='Community Feed' text='Community Feed' />
+    },
+    ...DefualtsMenu,
+    {
+      key: 'File Upload',
+      component: <FileUpload key='File Upload' />,
+      menuItem: <Item icon='fas-file-upload' key='File Upload' text='File Upload' />
+    },
+    {
+      key: 'Messages',
+      component: <Messages key='Messages' />,
+      menuItem: <Item icon='fa-envelope-open' key='Messages' text='Messages' />
+    }
+
+  ]
+  return menu
+}
+
+export default MenuComponents2
