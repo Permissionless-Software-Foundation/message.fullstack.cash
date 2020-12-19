3e6b4dbce381d0813ac6198871995533680c0399
Chris Troutner <chris.troutner@gmail.com>
2020-12-19 13:04:38 -0800

    fix(IPFS): Switching to new IPFS and tor syndication

--- src/gatsby-ipfs-web-wallet/components/site-config.js
+++ src/gatsby-ipfs-web-wallet/components/site-config.js
@@ -7,7 +7,18 @@ const config = {
   title: 'BCH Message',
   titleShort: 'MSG',
   balanceText: 'BCH Balance',
-  balanceIcon: 'fab-bitcoin'
+  balanceIcon: 'fab-bitcoin',
+
+  // The BCH address used in a memo.cash account. Used for tracking the IPFS
+  // hash of the mirror of this site.
+  memoAddr: 'bitcoincash:qrlur8kg4cvk62wy85pk06tyvvzddpae9cspggk87c',
+
+  // Footer Information
+  hostText: 'FullStack.cash',
+  hostUrl: 'https://fullstack.cash/',
+  sourceCode: 'https://github.com/Permissionless-Software-Foundation/message.fullstack.cash',
+  torUrl: '2qeeub5mshokrthmnb3mzcb5ah2lqzkz4fbdx6er4nhxags2pfkyoqid.onion',
+  clearWebUrl: 'https://message.FullStack.cash'
 }
 
 module.exports = config
