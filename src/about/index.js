a7abcf21c57fabcbd34e55dcda95c3d0b3abc596
Chris Troutner <chris.troutner@gmail.com>
2021-01-27 15:04:09 -0800

    Customizing the about page

--- src/about/index.js
+++ src/about/index.js
@@ -9,71 +9,98 @@ class About extends React.Component {
   render () {
     return (
       <Content
-        title='About'
-        subTitle=''
-        browserTitle='About | wallet.fullstack.cash'
+        title="About"
+        subTitle=""
+        browserTitle="About | Message.FullStack.Cash"
       >
         <Row>
           <Col xs={12}>
             <Box
-              title='wallet.fullstack.cash'
-              type='primary'
+              title="Message.FullStack.Cash"
+              type="primary"
               closable
               collapsable
             >
               <p>
-                This is the official web wallet provided by{' '}
+                Message.FullStack.Cash is the primary communication tool for the{' '}
                 <a
-                  href='https://fullstack.cash'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://PSFoundation.cash"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  FullStack.cash
-                </a>
-                . You can access this web wallet over Tor and IPFS. See the
-                links in the footer at the bottom of the page.
+                  Permissionless Software Foundation
+                </a>{' '}
+                (PSF). Here are the primary features of this app:
               </p>
+              <ul>
+                <li>
+                  It displays a community feed, taken from the Bitcoin Cash
+                  (BCH) blockchain.
+                </li>
+                <li>
+                  It allows anyone to send an end-to-end encrypted (e2ee) email
+                  to any BCH address.
+                </li>
+                <li>
+                  It allows hosting ('pinning') of IPFS files, paid with BCH.
+                </li>
+                <li>
+                  It allows cryptographic signing of messages. This is used to
+                  prove ownership of an address, to gain access to{' '}
+                  <a
+                    href="https://t.me/psf_vip"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    the PSF VIP Telegram channel
+                  </a>
+                </li>
+              </ul>
               <p>
-                This wallet app is currently in an 'Open Alpha' test. The
-                general public is invited to use it, but be aware that the app
-                is in-progress, the code is in-flux, and many things will change
-                or break. Please solicit feedback on this app in our{' '}
                 <a
-                  href='https://t.me/permissionless_software'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://youtu.be/KOlM4dU6Gj0"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  Telegram Channel
+                  Here is a video showing how to use the different PSF
+                  communication channels.
                 </a>
               </p>
+
               <p>
-                The code for this web wallet can be found{' '}
+                You can access this web wallet over Tor and IPFS. See the links
+                in the footer at the bottom of the page. The code for this web
+                wallet can be found{' '}
                 <a
-                  href='https://github.com/Permissionless-Software-Foundation/wallet.fullstack.cash'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://github.com/Permissionless-Software-Foundation/message.fullstack.cash"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   here
                 </a>
-                . It's based on the{' '}
-                <a
-                  href='https://github.com/Permissionless-Software-Foundation/gatsby-ipfs-web-wallet'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  gatsby-ipfs-web-wallet Gatsby Theme
-                </a>
-                , and the{' '}
-                <a
-                  href='https://github.com/Permissionless-Software-Foundation/minimal-slp-wallet'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  minimal-slp-wallet
-                </a>{' '}
-                wallet engine. We encourage other businesses to copy and modify
-                the wallet code for their own use.
               </p>
+
+              <p>
+                This app is built on top of{' '}
+                <a
+                  href="https://github.com/Permissionless-Software-Foundation/bch-wallet-starter"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  bch-wallet-starter
+                </a>
+                , the Gatsby Starter that includes basic BCH wallet
+                functionality. You can build your own app using that software
+                too.{' '}
+                <a
+                  href="https://youtu.be/G7ptg7VIRnk"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Here is a video showing how.
+                </a>
+              </p>
+
               <p>
                 This wallet has full support for Bitcoin Cash and SLP tokens,
                 including nonfungible tokens (NFT). When a transaction is made
@@ -82,9 +109,9 @@ class About extends React.Component {
                 support the developers who work on this web wallet and other
                 software sponsored by the{' '}
                 <a
-                  href='https://psfoundation.cash'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://psfoundation.cash"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   Permissionless Software Foundation
                 </a>
